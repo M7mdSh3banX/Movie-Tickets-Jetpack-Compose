3c7bbc01f57315f8f55876daeadead0ad60411ef
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-07-03 22:50:27 +0300

    add poppins font family

---
+++ app/src/main/java/com/shaban/movietickets/ui/theme/Font.kt
@@ -0,0 +1,11 @@
+package com.shaban.movietickets.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.shaban.movietickets.R
+
+val Poppins = FontFamily(
+    Font(resId = R.font.poppins_regular, weight = FontWeight.Normal),
+    Font(resId = R.font.poppins_medium, weight = FontWeight.Medium)
+)
