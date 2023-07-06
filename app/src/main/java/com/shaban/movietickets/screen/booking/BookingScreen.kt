306080330618e605a9f30c48e48c30afba5cb679
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-07-06 23:19:34 +0300

    update booking screen with

--- app/src/main/java/com/shaban/movietickets/screen/booking/BookingScreen.kt
+++ app/src/main/java/com/shaban/movietickets/screen/booking/BookingScreen.kt
@@ -1,6 +1,7 @@
 package com.shaban.movietickets.screen.booking
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,16 +10,17 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.shape.AbsoluteRoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -48,14 +50,13 @@ fun BookingScreen(viewModel: BookingViewModel = hiltViewModel()) {
 private fun BookingScreenContent(
     state: BookingUiState
 ) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
+    Box(
         modifier = Modifier.fillMaxSize()
     ) {
         Box(
             modifier = Modifier
-                .fillMaxWidth()
-                .weight(0.4f)
+                .align(Alignment.TopCenter)
+                .height(420.dp),
         ) {
             MovieImage(data = state.movieImage)
             BookingHeader(movieDuration = state.movieDuration)
@@ -63,59 +64,64 @@ private fun BookingScreenContent(
                 PlayMovie {}
             }
         }
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .weight(0.6f)
-                .clip(RoundedCornerShape(topStart = 32.dp, topEnd = 32.dp)),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Row(
+        Box(modifier = Modifier.align(Alignment.BottomCenter)) {
+            Column(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(horizontal = 16.dp, vertical = 16.dp),
-                horizontalArrangement = Arrangement.SpaceEvenly,
-                verticalAlignment = Alignment.CenterVertically
+                    .height(450.dp)
+                    .background(
+                        shape = AbsoluteRoundedCornerShape(topLeft = 32.dp, topRight = 32.dp),
+                        color = White
+                    ),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                RatingText(rating = state.imdbRating, text = stringResource(R.string.imdb))
-                RottenTomatoesRatingText(
-                    rating = state.rottenTomatoesRating,
-                    text = stringResource(R.string.rotten_tomatoes)
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 16.dp),
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    RatingText(rating = state.imdbRating, text = stringResource(R.string.imdb))
+                    RottenTomatoesRatingText(
+                        rating = state.rottenTomatoesRating,
+                        text = stringResource(R.string.rotten_tomatoes)
+                    )
+                    RatingText(rating = state.ignRating, text = stringResource(R.string.ign))
+                }
+                MovieName(name = state.movieName)
+                LazyRow(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 4.dp),
+                    horizontalArrangement = Arrangement.Center,
+                ) {
+                    items(state.movieTags) { tag ->
+                        MovieTag(name = tag)
+                    }
+                }
+                SpacerVertical4()
+                LazyRow(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    contentPadding = PaddingValues(horizontal = 16.dp)
+                ) {
+                    items(state.movieCast) { image ->
+                        MovieCastItem(data = image)
+                    }
+                }
+                SpacerVertical4()
+                MovieDescription(description = state.movieDescription)
+                SpacerVertical4()
+                Spacer(modifier = Modifier.weight(1F))
+                CustomButton(
+                    buttonText = stringResource(R.string.booking),
+                    painter = painterResource(id = R.drawable.booking_icon),
+                    onClickBooking = { },
+                    modifier = Modifier
                 )
-                RatingText(rating = state.ignRating, text = stringResource(R.string.ign))
+                SpacerVertical16()
             }
-            MovieName(name = state.movieName)
-            LazyRow(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp, vertical = 4.dp),
-                horizontalArrangement = Arrangement.Center,
-            ) {
-                items(state.movieTags) { tag ->
-                    MovieTag(name = tag)
-                }
-            }
-            SpacerVertical4()
-            LazyRow(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.spacedBy(8.dp),
-                contentPadding = PaddingValues(horizontal = 16.dp)
-            ) {
-                items(state.movieCast) { image ->
-                    MovieCastItem(data = image)
-                }
-            }
-            SpacerVertical4()
-            MovieDescription(description = state.movieDescription)
-            SpacerVertical4()
-            Spacer(modifier = Modifier.weight(1F))
-            CustomButton(
-                buttonText = stringResource(R.string.booking),
-                painter = painterResource(id = R.drawable.booking_icon),
-                onClickBooking = { },
-                modifier = Modifier
-            )
-            SpacerVertical16()
         }
     }
 }
